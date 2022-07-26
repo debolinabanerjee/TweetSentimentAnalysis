81c7fa211d61d747a0ba99141bde185d46901f9b
Debolina Banerjee <debolina.ad2024@gmail.com>
2022-07-27 00:09:48 +0530

    First Commit

---
+++ config.py
@@ -0,0 +1,9 @@
+# api_key = 'AVpiv5izhg6RScPYHjhuO7UcE'
+# api_key_secret  = '9ujrToN4pYLeTSgTm2JV0eEiI8AZPCQ3Bh620FH1odrnYpTeuS'
+# access_token = '1542210775766278145-Vo8860x5tCjtB2faTPK5U6BAdPIksM'
+# access_token_secret = 'hqZPR4aMiPBTk1wWQuk59jzyUFZf8rZI3NoZTaDbI0CCg'
+#De's key 
+api_key = 'jKJTfvyYQrt4hmg4sEj6obuhJ'
+api_key_secret  ='0UWViEHTQW97TLWVb2cPBGGXTHNTppzmNBk96okSVKVeUntCIB'
+access_token ='1542204749738811392-0SWaqlu2yjFcFxi61KFyrv7aashmfU'
+access_token_secret ='JY7y1CzTbbzfWuN4pVQYKbMUakHZujcYy60y5XfObZPdD'
